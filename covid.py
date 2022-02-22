17def3d8b2be8592701602807e5cc3e2f558e26c
KeitaOsada <82925549+KeitaOsada@users.noreply.github.com>
2022-02-23 01:02:33 +0900

    2020/2/23

--- covid.py
+++ covid.py
@@ -1,89 +1,131 @@
+import argparse
 import datetime
-import pandas as pd
-import matplotlib.pyplot as plt
-from tqdm import tqdm
 import os
 import shutil
-import sys
+
+import japanize_matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib import tight_layout
+from tqdm import tqdm
 
 
+def get_data():
+    # 日別の新規感染者データ
+    confirmed_df = pd.read_csv(
+        "https://covid19.mhlw.go.jp/public/opendata/newly_confirmed_cases_daily.csv"
+    )
+    # 日別の重症者数データ
+    severe_df = pd.read_csv(
+        "https://covid19.mhlw.go.jp/public/opendata/severe_cases_daily.csv"
+    )
+    # 都道府県のset
+    prefectures = list(confirmed_df.columns)
+    prefectures.remove("Date")
 
-plt.rcParams['font.family'] = "MS Mincho"
-plt.rcParams["font.size"]=18
+    return confirmed_df, severe_df, prefectures
 
-#日別の新規感染者データ
-confirmed_DF=pd.read_csv("https://covid19.mhlw.go.jp/public/opendata/newly_confirmed_cases_daily.csv")
-#日別の重症者数データ
-severe_DF=pd.read_csv("https://covid19.mhlw.go.jp/public/opendata/severe_cases_daily.csv")
-#都道府県のset
-prefectures=list(confirmed_DF.columns)
-prefectures.remove("Date")
 
-def dataProcessing():
+def data_processing(confirmed_df, severe_df, start_date="2020/5/9"):
     # severe_DF starts 2020/5/9
-    global confirmed_DF
-    index=0
-    for i in range(len(confirmed_DF)):
-        if confirmed_DF.Date[i]=="2020/5/9":
-            index=i
-            break
-    confirmed_DF=confirmed_DF.drop(confirmed_DF.index[0:index]).reset_index(drop=True)
+    assert start_date in list(confirmed_df.Date)
+    assert start_date in list(severe_df.Date)
 
-def calSevereRate():
-    confirms={prefecture:list(confirmed_DF[prefecture]) for prefecture in prefectures}
-    severes={prefecture:list(severe_DF[prefecture])for prefecture in prefectures}
-    rates={prefecture:[severe/confirm if confirm!=0 else 100 for severe,confirm in zip(severes.get(prefecture),confirms.get(prefecture))]for prefecture in confirms.keys()}
+    confirmed_df = confirmed_df[
+        confirmed_df.index >= list(confirmed_df.Date).index(start_date)
+    ]
+    severe_df = severe_df[severe_df.index >= list(severe_df.Date).index(start_date)]
 
-    return confirms,severes,rates
+    return confirmed_df, severe_df
 
-def getDate():
-    d=datetime.datetime.now()
-    return str(d.year)+"-"+str(d.month)+"-"+str(d.day)
 
-def drawGraph(data,genre,pre,axes):
-    genre_jpn=""
-    if genre=="severe_rate":
-        genre_jpn="重症率"
-    elif genre=="confirm":
-        genre_jpn="新規感染者数"
-    elif genre=="severe":
-        genre_jpn="重症者数"
+def cal_severe_rate(confirmed_df, severe_df, prefectures, error_num=100):
+    confirms = {
+        prefecture: list(confirmed_df[prefecture]) for prefecture in prefectures
+    }
+    severes = {prefecture: list(severe_df[prefecture]) for prefecture in prefectures}
+    rates = {
+        prefecture: [
+            severe / confirm if confirm != 0 else error_num
+            for severe, confirm in zip(
+                severes.get(prefecture), confirms.get(prefecture)
+            )
+        ]
+        for prefecture in confirms.keys()
+    }
 
-    axes.plot(pd.date_range('2020-05-09 00:00:00', periods=len(data), freq='d'),data)
-    # plt.xticks(np.arange(0,len(data),30))
-    if genre=="severe_rate":
-        axes.set_ylim([0,1])#だいたいは(重症者数)<(新規感染者数)の為
+    return confirms, severes, rates
+
+
+def get_date():
+    d = datetime.datetime.now()
+    return str(d.year) + "/" + str(d.month) + "/" + str(d.day)
+
+
+def plot_data(data, prefecture, genre, axes, start_date):
+    axes.plot(
+        pd.date_range(
+            start_date.replace("/", "-") + " 00:00:00", periods=len(data), freq="d"
+        ),
+        data,
+    )
+    if genre == "重症率":
+        axes.set_ylim([0, 1])  # だいたいは(重症者数)<(新規感染者数)の為
     axes.grid()
-    axes.set_title("2020-5-9から"+getDate()+"までの"+genre_jpn+"の推移("+pre+")")
-    axes.set_xlabel("日付")
-    
+    axes.set_title(
+        "2020-5-9から" + get_date() + "までの" + genre + "の推移(" + prefecture + ")",
+        fontsize=18,
+    )
+    axes.set_xlabel("日付", fontsize=18)
+    axes.set_ylabel(genre, fontsize=18)
 
-def drawAndSaveAllGraphs(c_dict,s_dict,r_dict,output_dirname):
+
+def draw_graph(confirm, severe, rate, prefecture, output_dirname, start_date):
+    subplot_num = 311
+    figure = plt.figure(1, figsize=(20, 25))
+    genres = ["新規感染者数", "重症者数", "重症率"]
+    for data, genre in zip([confirm, severe, rate], genres):
+        plot_data(data, prefecture, genre, figure.add_subplot(subplot_num), start_date)
+        subplot_num += 1
+
+    plt.savefig(f"{output_dirname}{prefecture}.png")
+    plt.close()
+
+
+def draw_and_save_all_graphs(
+    c_dict, s_dict, r_dict, prefectures, output_dirname, start_date="2020/5/9"
+):
     if os.path.isdir(output_dirname):
         shutil.rmtree(output_dirname)
-    
+
     os.makedirs(output_dirname)
 
-    for p in tqdm(c_dict.keys()):
+    for prefecture in tqdm(prefectures):
         # print(p)
-        confirm=c_dict.get(p)
-        severe=s_dict.get(p)
-        rate=r_dict.get(p)
-        figure= plt.figure(1, figsize=(20,25))
-        ax = figure.add_subplot(311)
-        drawGraph(confirm,"confirm",p,ax)
-        ax = figure.add_subplot(312)
-        drawGraph(severe,"severe",p,ax)
-        ax = figure.add_subplot(313)
-        drawGraph(rate,"severe_rate",p,ax)
-        plt.savefig("images/"+p+".png")
-        plt.close()
+        confirm = c_dict.get(prefecture)
+        severe = s_dict.get(prefecture)
+        rate = r_dict.get(prefecture)
+        draw_graph(confirm, severe, rate, prefecture, output_dirname, start_date)
 
-def main():
-    output_dirname="./images"
-    dataProcessing()
-    confirms,severes,rates=calSevereRate()
-    drawAndSaveAllGraphs(confirms,severes,rates,output_dirname)
+
+def main(args):
+    start_date = args.start_date
+    error_num = args.error_num
+    output_dirname = args.output
+
+    confirmed_df, severe_df, prefectures = get_data()
+    confirmed_df, severe_df = data_processing(confirmed_df, severe_df, start_date)
+    confirms, severes, rates = cal_severe_rate(
+        confirmed_df, severe_df, prefectures, error_num
+    )
+    draw_and_save_all_graphs(
+        confirms, severes, rates, prefectures, output_dirname, start_date
+    )
+
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output", type=str, help="出力先のディレクトリ", default="./images/")
+    parser.add_argument("--error_num", type=int, help="新規感染者数が0だった時の重症率の値", default=100)
+    parser.add_argument("--start_date", type=str, help="描画を開始する日付", default="2020/5/9")
+    main(parser.parse_args())
